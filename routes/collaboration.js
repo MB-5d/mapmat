8311740a5d439a1b39d2fe99c607b0ea627d5e4f
MB-5d <mattbraundesign@gmail.com>
2026-07-28 05:14:28 -0400

    Modernize backend dependencies and runtime

--- routes/collaboration.js
+++ routes/collaboration.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('node:crypto');
 const { authMiddleware, requireAuth } = require('./auth');
 const mapStore = require('../stores/mapStore');
 const authStore = require('../stores/authStore');
