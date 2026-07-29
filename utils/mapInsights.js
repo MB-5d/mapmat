cf33b2d9f9fe7333df837ebe4f8a109d23aac2e4
MB-5d <mattbraundesign@gmail.com>
2026-07-28 21:33:12 -0400

    Modernize backend dependencies and runtime

--- utils/mapInsights.js
+++ utils/mapInsights.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('node:crypto');
 const { detectChallengePage } = require('./scanPageClassification');
 const { isPageNode } = require('./mapScene');
 
